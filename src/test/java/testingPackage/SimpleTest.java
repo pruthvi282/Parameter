ea901f774afbf8116a4dd47ad6a13b936ae1de53
pruthvi282 <pruthvi282@gmail.com>
2021-08-07 22:15:07 +0530

    2nd commit

--- src/test/java/testingPackage/SimpleTest.java
+++ src/test/java/testingPackage/SimpleTest.java
@@ -16,10 +16,14 @@ public class SimpleTest {
 		if(name.equals("firefox")){
 		driver=new FirefoxDriver();
 		}
+		else if(name.equals("internet")){
+			System.out.println("hiiii");
+		}
 		else{
 			driver=new ChromeDriver()
 			//oay
 		}
+
 		driver.get("https://rahulshettyacademy.com/AutomationPractice/");
 	driver.findElement(By.xpath("//input[@class='inputs ui-autocomplete-input']")).sendKeys("this is a test");
 	driver.findElement(By.xpath("//input[@value='radio1']")).click();
