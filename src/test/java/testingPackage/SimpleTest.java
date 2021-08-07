4d822bc833fe0557cebf941662a1366d5c537a30
pruthvi282 <pruthvi282@gmail.com>
2021-08-07 22:09:24 +0530

    .kjhgfdgh

--- src/test/java/testingPackage/SimpleTest.java
+++ src/test/java/testingPackage/SimpleTest.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.SourceType;
 import org.testng.annotations.Test;
 
 public class SimpleTest {
@@ -11,12 +12,13 @@ public class SimpleTest {
 	@Test
 	public void test(){
 		WebDriver driver=null;
-		String name=System.getProperty("browser");
+		String name= System.getProperty("browser");
 		if(name.equals("firefox")){
 		driver=new FirefoxDriver();
 		}
 		else{
-			driver=new ChromeDriver();
+			driver=new ChromeDriver()
+			//oay
 		}
 		driver.get("https://rahulshettyacademy.com/AutomationPractice/");
 	driver.findElement(By.xpath("//input[@class='inputs ui-autocomplete-input']")).sendKeys("this is a test");
